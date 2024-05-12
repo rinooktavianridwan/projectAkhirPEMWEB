64aa889c4e393f2d5f14cb989c9793d972d30cde
Wiyu Warwoyo <bobobobi.krn@gmail.com>
2024-05-12 19:12:21 +0700

    index ke profile dah bisa

--- resources/views/layouts/Guest/headerGuest.blade.php
+++ resources/views/layouts/Guest/headerGuest.blade.php
@@ -7,12 +7,24 @@
             <div class="homepage">Homepage</div>
             <div class="about-us">About Us</div>
             <div class="cars">Cars</div>
-            <div class="my-account">My Account</div>
+            <div class="my-account">
+                @if (Route::has('login'))
+                    @auth
+                        <a href="{{ url('/dashboard') }}">
+                            My Account
+                        </a>
+                    @else
+                        <a href="{{ route('login') }}">
+                            My Account
+                        </a>
+                    @endauth
+                @endif
+            </div>
             <div class="tombolLogin" >
             @if (Route::has('login'))
                 @auth
                     <a href="{{ url('/dashboard') }}">
-                        Dashboard
+                        log out
                     </a>
                 @else
                     <a href="{{ route('login') }}">
