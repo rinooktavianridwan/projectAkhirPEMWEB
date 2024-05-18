a069ab59d39c89120fcd9662b8f254dc4c41c193
Rino <rinooktavianridwan@gmail.com>
2024-05-18 18:47:41 +0700

    tampilan transaction

--- resources/views/admin/homeAdmin.blade.php
+++ resources/views/admin/homeAdmin.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,10 +9,11 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
     <title>AdminHub</title>
-    
+
 </head>
+
 <body>
-<section id="sidebar">
+    <section id="sidebar">
         <a href="#" class="brand">
             <i class='bx bxs-smile'></i>
             <span class="text">Speedy Admin</span>
@@ -29,7 +31,7 @@
                     <span class="text">Cars</span>
                 </a>
             </li>
-			<li>
+            <li>
                 <a href="{{ route('dashboard') }}">
                     <i class='bx bxs-group'></i>
                     <span class="text">dashboard</span>
@@ -47,101 +49,102 @@
             </li>
         </ul>
     </section>
-    
+
     <section id="content">
-    <main>
-			<div class="head-title">
-				<div class="left">
-					<h1>Dashboard</h1>
-					<ul class="breadcrumb">
-						<li>
-							<a href="#">Dashboard</a>
-						</li>
-						<li><i class='bx bx-chevron-right' ></i></li>
-						<li>
-							<a class="active" href="#">Home</a>
-						</li>
-					</ul>
-				</div>
-				<a href="#" class="btn-download">
-					<i class='bx bxs-cloud-download' ></i>
-					<span class="text">Download PDF</span>
-				</a>
-			</div>
+        <main>
+            <div class="head-title">
+                <div class="left">
+                    <h1>Dashboard</h1>
+                    <ul class="breadcrumb">
+                        <li>
+                            <a href="#">Dashboard</a>
+                        </li>
+                        <li><i class='bx bx-chevron-right'></i></li>
+                        <li>
+                            <a class="active" href="#">Home</a>
+                        </li>
+                    </ul>
+                </div>
+                <a href="#" class="btn-download">
+                    <i class='bx bxs-cloud-download'></i>
+                    <span class="text">Download PDF</span>
+                </a>
+            </div>
 
-			<ul class="box-info">
-				<li>
-					<i class='bx bxs-calendar-check' ></i>
-					<span class="text">
-						<h3> {{$banyakTransaksi}}</h3>
-						<p>New Order</p>
-					</span>
-				</li>
-				<li>
-					<i class='bx bxs-group' ></i>
-					<span class="text">
-						<h3>2834</h3>
-						<p>Visitors</p>
-					</span>
-				</li>
-				<li>
-					<i class='bx bxs-dollar-circle' ></i>
-					<span class="text">
-			
-						<h3>Rp{{ number_format($total, 0, ',', '.') }}</h3>
-						<p>Total Sales</p>
-					</span>
-				</li>
-			</ul>
+            <ul class="box-info">
+                <li>
+                    <i class='bx bxs-calendar-check'></i>
+                    <span class="text">
+                        <h3> {{ $banyakTransaksi }}</h3>
+                        <p>New Order</p>
+                    </span>
+                </li>
+                <li>
+                    <i class='bx bxs-group'></i>
+                    <span class="text">
+                        <h3>2834</h3>
+                        <p>Visitors</p>
+                    </span>
+                </li>
+                <li>
+                    <i class='bx bxs-dollar-circle'></i>
+                    <span class="text">
+
+                        <h3>Rp{{ number_format($total, 0, ',', '.') }}</h3>
+                        <p>Total Sales</p>
+                    </span>
+                </li>
+            </ul>
 
 
-			<div class="table-data">
-				<div class="order">
-					<div class="head">
-						<h3>Recent Orders</h3>
-						<i class='bx bx-search' ></i>
-						<i class='bx bx-filter' ></i>
-					</div>
-					<table>
-						<thead>
-							<tr>
-								<th>User</th>
-								<th>Date Order</th>
-								<th>Status</th>
-							</tr>
-						</thead>
-						<tbody>
-							<!-- tabel recent order -->
-							@foreach($transactions as $transaction)
-							<tr>
-								<td>
-									<img src="/storage/{{ $transaction->car->image }}" alt="user" />
-									<p>{{ $transaction->user->name }}</p>
-									<p>{{ $transaction->id }}</p>
-									<p>{{ $transaction->car->name }}</p>
-									<p>{{ $transaction->car->city }}</p>
-								</td>
-								<td>{{ $transaction->updated_at }}</td>
-								<td>
-									@if (now() < $transaction->pickup_date)
-										Booked
-									@elseif (now() >= $transaction->pickup_date && now() < $transaction->return_date)
-										Sedang Digunakan
-									@else
-										Done
-									@endif
-								</td>
-							</tr>
-							@endforeach
-						</tbody>
-					</table>
-				</div>
-			</div>
-		</main>
-        
-        
+            <div class="table-data">
+                <div class="order">
+                    <div class="head">
+                        <h3>Recent Orders</h3>
+                    </div>
+                    <table>
+                        <thead>
+                            <th>Transaction</th>
+                            <th>Date Order</th>
+                            <th>Use</th>
+                            <th>Status</th>
+                        </thead>
+                        <tbody>
+                            <!-- tabel recent order -->
+                            @foreach ($transactions as $transaction)
+                                <tr>
+                                    <td>
+                                        <img class="img-car-admin" src="/storage/{{ $transaction->car->image }}"
+                                            alt="user" />
+                                        <p>Id Transaksi : {{ $transaction->id }}</p>
+                                        <p>Kategori Mobil : {{ $transaction->car->kategori }}</p>
+                                        <p>Kota : {{ $transaction->car->city }}</p>
+                                        <p>Nama Pemesan : {{ $transaction->user->name }}</p>
+                                        <p>No Telepon : {{ $transaction->user->phone }}</p>
+                                    </td>
+                                    <td>{{ $transaction->updated_at }}</td>
+                                    <td>{{ $transaction->pickup_date }}-{{ $transaction->return_date }}</td>
+                                    <td>
+                                        @if (now() < $transaction->pickup_date)
+                                            Booked
+                                        @elseif (now() >= $transaction->pickup_date && now() < $transaction->return_date)
+                                            Sedang Digunakan
+                                        @else
+                                            Done
+                                        @endif
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </main>
+
+
     </section>
 
-  
+
 </body>
+
 </html>
